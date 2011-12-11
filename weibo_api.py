3ce1b2fa3cac8217cf3954795e883af336e8f636
diamrem <diamrem@ibeca.me>
2011-12-11 14:45:27 -0500

    Update weibo_api.py

--- weibo_api.py
+++ weibo_api.py
@@ -1,13 +1,13 @@
 from weibopy.auth import OAuthHandler, BasicAuthHandler
 from weibopy.api import API
 
-password = '86842128'
-username = 'reg.1@lifeis.ws'
-app_key = '3535699815'
+password = 'your password'
+username = 'your username'
+app_key = 'your appkey'
 
 def get_api():
-    auth = BasicAuthHandler('reg.1@lifeis.ws', '86842128')
-    api = API(auth, source = '3535699815')
+    auth = BasicAuthHandler(username, password)
+    api = API(auth, source = app_key)
     return api
 
 
